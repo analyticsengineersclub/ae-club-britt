138f31d8ff814764738be29557073f2858efd4a2
Ubuntu <ubuntu@ip-172-31-17-42.us-east-2.compute.internal>
2022-03-03 06:09:58 +0000

    Adding pre-work customers SQL query

---
+++ customers.sql
@@ -0,0 +1,12 @@
+select 
+      o.customer_id
+      , c.name
+      , c.email
+      , min(o.created_at) first_order_at
+      , count(o.id) number_of_orders
+from analytics-engineers-club.coffee_shop.customers c
+join analytics-engineers-club.coffee_shop.orders o
+on o.customer_id = c.id
+group by 1, 2, 3
+order by first_order_at
+limit 5
