55a8413e150f202464e75e46901d8bab4118ce33
Ubuntu <ubuntu@ip-172-31-17-42.us-east-2.compute.internal>
2022-03-03 09:05:45 +0000

    Removed aliases to use more useful and clear table names

--- customers.sql
+++ customers.sql
@@ -1,12 +1,12 @@
 select 
-      o.customer_id
-      , c.name
-      , c.email
-      , min(o.created_at) first_order_at
-      , count(o.id) number_of_orders
-from analytics-engineers-club.coffee_shop.customers c
-join analytics-engineers-club.coffee_shop.orders o
-on o.customer_id = c.id
+      orders.customer_id
+      , customers.name
+      , customers.email
+      , min(orders.created_at) first_order_at
+      , count(orders.id) number_of_orders
+from analytics-engineers-club.coffee_shop.customers
+join analytics-engineers-club.coffee_shop.orders
+on orders.customer_id = customers.id
 group by 1, 2, 3
 order by first_order_at
 limit 5
